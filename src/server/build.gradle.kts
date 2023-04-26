a3b4ceb5d3ca5d1c43b8b6329f09896b737fc516
이서현 <ymecca12@gmail.com>
2023-04-26 21:03:58 +0900

    build : 초기 개발환경 구축
    build : 개발 폴더 추가
    
    build : 개발 폴더 추가

---
+++ src/server/build.gradle.kts
@@ -0,0 +1,47 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("org.springframework.boot") version "2.7.11" apply false
+    id("io.spring.dependency-management") version "1.0.15.RELEASE" apply false
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21" apply false
+    kotlin("plugin.jpa") version "1.6.21" apply false
+}
+
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+
+allprojects{
+    group = "com.depromeet"
+    version = "0.0.1-SNAPSHOT"
+
+    repositories {
+        mavenCentral()
+    }
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "11"
+        }
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+    }
+
+}
+
+subprojects{
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "org.springframework.boot")
+    apply(plugin = "io.spring.dependency-management")
+
+    dependencies {
+        implementation("org.springframework.boot:spring-boot-starter-web")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+        implementation("org.jetbrains.kotlin:kotlin-reflect")
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+    }
+}
---
+++ src/server/promise-api/src/main/kotlin/com/depromeet/promiseapi/application/port/out/RecordDomainPort.kt
@@ -0,0 +1,5 @@
+package com.depromeet.promiseapi.application.port.out
+
+open interface RecordDomainPort {
+    fun recordDomain()
+}
