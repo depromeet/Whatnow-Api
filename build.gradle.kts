b5e31cba062391a475594f9b1a7ed8054738f8d9
이찬진 <water0641@naver.com>
2023-05-08 14:37:55 +0900

    chore :  gradle jacoco test dependson 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,10 +6,27 @@ plugins {
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21" apply false
     kotlin("plugin.jpa") version "1.6.21" apply false
-    id("com.diffplug.spotless") version  "6.18.0"
+    id("com.diffplug.spotless") version  "6.18.0" // spotless
+    id("org.sonarqube") version "3.5.0.2730" // 소나 클라우드
+    id("jacoco")
 }
 
 java.sourceCompatibility = JavaVersion.VERSION_11
+
+sonarqube {
+    properties {
+        property ("sonar.projectKey", "depromeet_Whatnow-Api")
+        property ("sonar.organization", "depromeet-1")
+        property ("sonar.host.url", "https://sonarcloud.io")
+        property ("sonar.sources", "src")
+        property ("sonar.language", "java")
+        property ("sonar.sourceEncoding", "UTF-8")
+        property ("sonar.test.inclusions", "**/*Test.java")
+        property ("sonar.exclusions", "**/test/**, **/Q*.java, **/*Doc*.java, **/resources/** ,**/*Application*.java , **/*Config*.java, **/*Dto*.java, **/*Request*.java, **/*Response*.java ,**/*Exception*.java ,**/*ErrorCode*.java")
+        property ("sonar.java.coveragePlugin", "jacoco")
+    }
+}
+
 
 allprojects {
     group = "com.depromeet"
@@ -28,6 +45,7 @@ allprojects {
     tasks.withType<Test> {
         useJUnitPlatform()
     }
+
 }
 
 configure<com.diffplug.gradle.spotless.SpotlessExtension> {
@@ -39,11 +57,15 @@ configure<com.diffplug.gradle.spotless.SpotlessExtension> {
     }
 }
 
+
+
 subprojects{
     apply(plugin = "org.jetbrains.kotlin.jvm")
     apply(plugin = "org.jetbrains.kotlin.plugin.spring")
     apply(plugin = "org.springframework.boot")
     apply(plugin = "io.spring.dependency-management")
+    apply(plugin = "jacoco" )
+
 
     dependencies {
         implementation("org.springframework.boot:spring-boot-starter-web")
@@ -54,5 +76,13 @@ subprojects{
     }
     tasks.getByName<Jar>("jar") {
         enabled = false
+    }
+
+
+    tasks.test {
+        finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+    }
+    tasks.jacocoTestReport {
+        dependsOn(tasks.test) // tests are required to run before generating the report
     }
 }
