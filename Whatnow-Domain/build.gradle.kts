5e140f923822d1c1c5f30629e63ff452f02c739b
이찬진 <water0641@naver.com>
2023-05-11 00:50:05 +0900

    chore : 임포트 대상 모듈 bootjar disable 시키기

--- Whatnow-Domain/build.gradle.kts
+++ Whatnow-Domain/build.gradle.kts
@@ -1,6 +1,15 @@
-import org.jetbrains.kotlin.ir.backend.js.compile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
 
 dependencies {
     api("org.springframework.boot:spring-boot-starter-data-jpa")
     api("org.springframework.boot:spring-boot-starter-data-redis")
+    api("com.h2database:h2")
+}
+
+tasks.getByName<BootJar>("bootJar") {
+    enabled = false
+}
+
+tasks.getByName<Jar>("jar") {
+    enabled = true
 }
--- Whatnow-Infrastructure/build.gradle.kts
+++ Whatnow-Infrastructure/build.gradle.kts
@@ -1,33 +1,9 @@
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.springframework.boot.gradle.tasks.bundling.BootJar
 
-plugins {
-    id("org.springframework.boot") version "2.7.11"
-    id("io.spring.dependency-management") version "1.0.15.RELEASE"
-    kotlin("jvm") version "1.6.21"
-    kotlin("plugin.spring") version "1.6.21"
+tasks.getByName<BootJar>("bootJar") {
+    enabled = false
 }
 
-group = "com.depromeet"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
-
-repositories {
-    mavenCentral()
-}
-
-dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
-}
-
-tasks.withType<KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
-    }
-}
-
-tasks.withType<Test> {
-    useJUnitPlatform()
+tasks.getByName<Jar>("jar") {
+    enabled = true
 }
