ccf12f242b6c09559c4ae98631e7e4bf05bfec9e
이찬진 <water0641@naver.com>
2023-05-11 19:03:57 +0900

    fix :빌드오류 수정

--- Whatnow-Api/src/main/kotlin/com/depromeet/whatnow/user/usecase/RegisterUserUseCase.kt
+++ Whatnow-Api/src/main/kotlin/com/depromeet/whatnow/user/usecase/RegisterUserUseCase.kt
@@ -1,6 +1,5 @@
 package com.depromeet.whatnow.user.usecase
 
-import com.depromeet.whatnow.api.TestFeignClient
 import com.depromeet.whatnow.domains.user.domain.User
 import com.depromeet.whatnow.domains.user.service.UserDomainService
 import org.springframework.stereotype.Service
@@ -8,7 +7,6 @@ import org.springframework.stereotype.Service
 @Service // TODO : UseCase annotation 을 커스텀 하게 만들고싶은데 위치를 어디로 줘야할지? 커먼 레이어가없음
 class RegisterUserUseCase(
     val userDomainService: UserDomainService,
-    val testFeignClient: TestFeignClient,
 ) {
     fun execute(): User {
         return userDomainService.registerUser()
