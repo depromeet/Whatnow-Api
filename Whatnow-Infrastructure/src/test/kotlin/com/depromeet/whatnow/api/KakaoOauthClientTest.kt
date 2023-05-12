af61dbdea86ffadbbe244f9e6e501cb69289409a
이찬진 <water0641@naver.com>
2023-05-12 21:02:52 +0900

    style : spotless apply

--- Whatnow-Infrastructure/src/test/kotlin/com/depromeet/whatnow/api/KakaoOauthClientTest.kt
+++ Whatnow-Infrastructure/src/test/kotlin/com/depromeet/whatnow/api/KakaoOauthClientTest.kt
@@ -7,7 +7,6 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.cache.annotation.EnableCaching
 import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
