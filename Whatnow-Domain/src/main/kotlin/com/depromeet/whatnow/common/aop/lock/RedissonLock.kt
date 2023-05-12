cbe6d5c91451939a2fed630c49b5a24705f5e6c5
이찬진 <water0641@naver.com>
2023-05-12 16:37:45 +0900

    feat : redisson Lock annotation

---
+++ Whatnow-Domain/src/main/kotlin/com/depromeet/whatnow/common/aop/lock/RedissonLock.kt
@@ -0,0 +1,13 @@
+package com.depromeet.whatnow.common.aop.lock
+
+import java.util.concurrent.TimeUnit
+
+@Target
+@Retention(AnnotationRetention.RUNTIME)
+annotation class RedissonLock(
+    val identifier: String,
+    val lockName: String,
+    val waitTime: Long = 30L,
+    val leaseTime: Long = 10L,
+    val timeUnit: TimeUnit = TimeUnit.SECONDS,
+)
