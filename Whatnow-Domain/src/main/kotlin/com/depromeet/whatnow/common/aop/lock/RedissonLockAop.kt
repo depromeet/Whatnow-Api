22ad4dc72e8af16cd8c653335128b71ee652d2ae
이찬진 <water0641@naver.com>
2023-05-12 16:48:26 +0900

    feat : 분산락 aop 설정

---
+++ Whatnow-Domain/src/main/kotlin/com/depromeet/whatnow/common/aop/lock/RedissonLockAop.kt
@@ -0,0 +1,62 @@
+package com.depromeet.whatnow.common.aop.lock
+
+import org.aspectj.lang.ProceedingJoinPoint
+import org.aspectj.lang.annotation.Around
+import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.reflect.MethodSignature
+import org.redisson.api.RedissonClient
+import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression
+import org.springframework.stereotype.Component
+import org.springframework.transaction.TransactionTimedOutException
+
+@Aspect
+@Component
+@ConditionalOnExpression("\${ableRedissonLock:true}")
+class RedissonLockAop(
+    private val redissonClient: RedissonClient,
+    private val redissonCallNewTransaction: RedissonCallNewTransaction,
+) {
+
+    @Around("@annotation(com.depromeet.whatnow.common.aop.lock.RedissonLock)")
+    fun lock(joinPoint: ProceedingJoinPoint): Any {
+        val signature = joinPoint.signature as MethodSignature
+        val method = signature.method
+        val redissonLock = method.getAnnotation(RedissonLock::class.java)
+        val baseKey: String = redissonLock.lockName
+        val dynamicKey = getDynamicKeyFromMethodArg(signature.parameterNames, joinPoint.args, redissonLock.identifier)
+
+        val rLock = redissonClient.getLock("$baseKey:$dynamicKey")
+
+        try {
+            val available = rLock.tryLock(redissonLock.waitTime, redissonLock.leaseTime, redissonLock.timeUnit)
+            if (!available) {
+                throw Exception("에러정책 이후 변할 예정")
+            }
+
+            return redissonCallNewTransaction
+                .proceed(joinPoint)
+        } catch (e: TransactionTimedOutException) {
+            throw e
+        } finally {
+            try {
+                rLock.unlock()
+            } catch (e: IllegalMonitorStateException) {
+//                log.error(e.toString() + baseKey + dynamicKey)
+                throw e
+            }
+        }
+    }
+
+    fun getDynamicKeyFromMethodArg(
+        methodParameterNames: Array<String>,
+        args: Array<Any>,
+        paramName: String,
+    ): String {
+        for (i in methodParameterNames.indices) {
+            if ((methodParameterNames[i] == paramName)) {
+                return args[i].toString()
+            }
+        }
+        throw Exception("에러정책 이후 변할 예정")
+    }
+}
