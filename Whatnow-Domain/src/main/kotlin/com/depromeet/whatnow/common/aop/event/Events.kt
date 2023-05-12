b8d85202eb962c840080bb20e3b559982a5279a2
이찬진 <water0641@naver.com>
2023-05-12 13:58:11 +0900

    feat: Events 객체추가

---
+++ Whatnow-Domain/src/main/kotlin/com/depromeet/whatnow/common/aop/event/Events.kt
@@ -0,0 +1,18 @@
+package com.depromeet.whatnow.common.aop.event
+
+import org.springframework.context.ApplicationEventPublisher
+
+class Events {
+    private val publisherLocal = ThreadLocal<ApplicationEventPublisher>()
+    fun raise(event: DomainEvent) {
+        publisherLocal.get().publishEvent(event)
+    }
+
+    fun setPublisher(publisher: ApplicationEventPublisher) {
+        publisherLocal.set(publisher)
+    }
+
+    fun reset() {
+        publisherLocal.remove()
+    }
+}
