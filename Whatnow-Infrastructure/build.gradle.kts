8d3ce5955888a966565d727eeaaad9e87acedada
이찬진 <water0641@naver.com>
2023-05-12 16:31:07 +0900

    chore : redisson config

--- Whatnow-Infrastructure/build.gradle.kts
+++ Whatnow-Infrastructure/build.gradle.kts
@@ -6,6 +6,8 @@ dependencies{
     api ("org.springframework.cloud:spring-cloud-starter-openfeign:3.1.4")
     api ("io.github.openfeign:feign-jackson:12.1")
     api ("org.springframework.boot:spring-boot-starter-data-redis")
+    api ("org.redisson:redisson:3.19.0")
+
 
     testImplementation ("org.springframework.cloud:spring-cloud-starter-contract-stub-runner:3.1.5")
     testImplementation ("org.springframework.cloud:spring-cloud-contract-wiremock:3.1.5")
