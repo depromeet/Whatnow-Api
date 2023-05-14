4bb8d180cea82b64b1151a8288c55801e7d43c48
이서현 <ymecca12@gmail.com>
2023-05-14 16:19:42 +0900

    feat : Global Exception 케이스 작성

--- Whatnow-Common/src/main/kotlin/com/depromeet/whatnow/consts/WhatNowStatic.kt
+++ Whatnow-Common/src/main/kotlin/com/depromeet/whatnow/consts/WhatNowStatic.kt
@@ -10,11 +10,15 @@ const val REFRESH_TOKEN = "REFRESH_TOKEN"
 const val KR_YES = "예"
 const val KR_NO = "아니요"
 const val MILLI_TO_SECOND = 1000
+const val SERVICE_UNAVAILABLE = 503
 const val BAD_REQUEST = 400
 const val UNAUTHORIZED = 401
 const val FORBIDDEN = 403
 const val NOT_FOUND = 404
+const val METHOD_NOT_ALLOWED = 405
 const val INTERNAL_SERVER = 500
+const val REQUEST_TIMEOUT = 408
+const val TOO_MANY_REQUESTS = 429
 const val ZERO = 0L
 const val KID = "kid"
 class WhatNowStatic
