66cd9c427fb534fa18434f5b95d71ac6efdb1208
이찬진 <water0641@naver.com>
2023-05-15 02:43:48 +0900

    feat : userAdapter 추가

---
+++ Whatnow-Domain/src/main/kotlin/com/depromeet/whatnow/domains/user/adapter/UserAdapter.kt
@@ -0,0 +1,16 @@
+package com.depromeet.whatnow.domains.user.adapter
+
+import com.depromeet.whatnow.domains.user.domain.User
+import com.depromeet.whatnow.domains.user.repository.UserRepository
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Component
+
+@Component
+class UserAdapter (
+    val userRepository: UserRepository
+        ){
+
+    fun queryUser(userId: Long): User {
+        return userRepository.findByIdOrNull(userId) ?: run { throw Error("유저없음 안됨") }
+    }
+}
