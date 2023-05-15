16fecc9d7caa638b0f2cdeba24e4f6c971de5835
이찬진 <water0641@naver.com>
2023-05-15 15:48:24 +0900

    feat : user me controller

--- Whatnow-Api/src/main/kotlin/com/depromeet/whatnow/api/auth/controller/AuthController.kt
+++ Whatnow-Api/src/main/kotlin/com/depromeet/whatnow/api/auth/controller/AuthController.kt
@@ -24,6 +24,7 @@ import javax.validation.Valid
 
 @RestController
 @RequestMapping("/v1/auth")
+@Tag(name = "1-1. [인증]")
 class AuthController(
     val registerUseCase: RegisterUserUseCase,
     val oauthUserInfoUseCase: OauthUserInfoUseCase,
