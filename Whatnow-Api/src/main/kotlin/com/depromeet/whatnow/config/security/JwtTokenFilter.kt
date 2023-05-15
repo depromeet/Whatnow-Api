1e2ebb854631573f37cf4ce0bf1e0ab9a0d75b75
이찬진 <water0641@naver.com>
2023-05-15 14:39:00 +0900

    feat : jwtToken filter 추가

---
+++ Whatnow-Api/src/main/kotlin/com/depromeet/whatnow/config/security/JwtTokenFilter.kt
@@ -0,0 +1,61 @@
+package com.depromeet.whatnow.config.security
+
+import com.depromeet.whatnow.config.consts.WhatnowStatic
+import com.depromeet.whatnow.config.jwt.AccessTokenInfo
+import com.depromeet.whatnow.config.jwt.JwtTokenHelper
+import com.depromeet.whatnow.config.static.AUTH_HEADER
+import com.depromeet.whatnow.config.static.BEARER
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import org.springframework.web.util.WebUtils
+import java.io.IOException
+import javax.servlet.FilterChain
+import javax.servlet.ServletException
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+class JwtTokenFilter (
+    val jwtTokenProvider: JwtTokenHelper
+        ) : OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain
+    ) {
+        val token = resolveToken(request)
+        if (token != null) {
+            val authentication = getAuthentication(token)
+            SecurityContextHolder.getContext().authentication = authentication
+        }
+        filterChain.doFilter(request, response)
+    }
+
+    private fun resolveToken(request: HttpServletRequest): String? {
+        // 쿠키방식 지원
+        val accessTokenCookie = WebUtils.getCookie(request, "accessToken")
+        if (accessTokenCookie != null) {
+            return accessTokenCookie.value
+        }
+        // 기존 jwt 방식 지원
+        val rawHeader = request.getHeader(AUTH_HEADER)
+        return if (rawHeader != null && rawHeader.length > BEARER.length && rawHeader.startsWith(
+               BEARER
+            )
+        ) {
+            rawHeader.substring(BEARER.length)
+        } else null
+    }
+
+    fun getAuthentication(token: String): Authentication {
+        val accessTokenInfo: AccessTokenInfo = jwtTokenProvider.parseAccessToken(token)
+        val userDetails: UserDetails =
+            AuthDetails(accessTokenInfo.getUserId().toString(), accessTokenInfo.getRole())
+        return UsernamePasswordAuthenticationToken(
+            userDetails, "user", userDetails.authorities
+        )
+    }
+}
