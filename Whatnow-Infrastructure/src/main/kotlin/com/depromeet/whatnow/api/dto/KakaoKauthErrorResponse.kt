f776f5814254778053f80e6d588a2a897f494b90
이찬진 <water0641@naver.com>
2023-05-16 19:19:05 +0900

    test : kauth 400 error test

--- Whatnow-Infrastructure/src/main/kotlin/com/depromeet/whatnow/api/dto/KakaoKauthErrorResponse.kt
+++ Whatnow-Infrastructure/src/main/kotlin/com/depromeet/whatnow/api/dto/KakaoKauthErrorResponse.kt
@@ -1,6 +1,5 @@
 package com.depromeet.whatnow.api.dto
 
-import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.PropertyNamingStrategies.SnakeCaseStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
