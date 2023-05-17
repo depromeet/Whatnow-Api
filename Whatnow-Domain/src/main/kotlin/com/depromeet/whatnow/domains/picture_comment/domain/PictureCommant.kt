aedca20f546b0cd6c512ecd76b0100e7b3330074
kdomo <dongho5088@naver.com>
2023-05-17 14:57:06 +0900

    feat: pictureComment entity 추가

---
+++ Whatnow-Domain/src/main/kotlin/com/depromeet/whatnow/domains/picture_comment/domain/PictureCommant.kt
@@ -0,0 +1,19 @@
+package com.depromeet.whatnow.domains.picture_comment.domain
+
+import com.depromeet.whatnow.common.BaseTimeEntity
+import javax.persistence.*
+
+@Entity
+@Table(name = "tbl_picture_comment")
+class PictureComment(
+    var pictureId: Long,
+
+    var userId: Long,
+
+    var comment: String,
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "picture_comment_id")
+    val id: Long? = null
+): BaseTimeEntity()
