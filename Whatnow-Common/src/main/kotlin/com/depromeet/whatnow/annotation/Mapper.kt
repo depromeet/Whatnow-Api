813c3988045c82ec9db91ba100eb0be25ea7228b
이찬진 <water0641@naver.com>
2023-05-20 15:26:15 +0900

    style : spotless

--- Whatnow-Common/src/main/kotlin/com/depromeet/whatnow/annotation/Mapper.kt
+++ Whatnow-Common/src/main/kotlin/com/depromeet/whatnow/annotation/Mapper.kt
@@ -2,12 +2,9 @@ package com.depromeet.whatnow.annotation
 
 import org.springframework.core.annotation.AliasFor
 import org.springframework.stereotype.Component
-import java.lang.annotation.Documented
-import java.lang.annotation.Retention
-import java.lang.annotation.RetentionPolicy
 
 @Target(AnnotationTarget.ANNOTATION_CLASS, AnnotationTarget.CLASS)
-@Retention(RetentionPolicy.RUNTIME)
-@Documented
+@Retention(AnnotationRetention.RUNTIME)
+@MustBeDocumented
 @Component
-annotation class Mapper(@AliasFor(annotation = Component::class) val value: String = "")
+annotation class Mapper(@get:AliasFor(annotation = Component::class) val value: String = "")
