f182dc0413fa5ec5b558e72bc645b73deb5a4ad2
kdomo <dongho5088@naver.com>
2023-05-30 20:12:41 +0900

    fix: S3Config에서 S3Properties 사용하도록 변경

--- Whatnow-Infrastructure/src/main/kotlin/com/depromeet/whatnow/config/s3/S3Config.kt
+++ Whatnow-Infrastructure/src/main/kotlin/com/depromeet/whatnow/config/s3/S3Config.kt
@@ -5,29 +5,20 @@ import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.client.builder.AwsClientBuilder
 import com.amazonaws.services.s3.AmazonS3
 import com.amazonaws.services.s3.AmazonS3ClientBuilder
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class S3Config(
-    @Value("\${ncp.s3.access-key}")
-    val accessKey: String,
-
-    @Value("\${ncp.s3.secret-key}")
-    val secretKey: String,
-
-    @Value("\${ncp.s3.region}")
-    val region: String,
-
-    @Value("\${ncp.s3.endpoint}")
-    val endPoint: String,
+    s3Properties: S3Properties,
 ) {
+    val s3Secret: S3Properties.S3Secret = s3Properties.s3
+
     @Bean
     fun amazonS3(): AmazonS3 {
         return AmazonS3ClientBuilder.standard()
-            .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration(endPoint, region))
-            .withCredentials(AWSStaticCredentialsProvider(BasicAWSCredentials(accessKey, secretKey)))
+            .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration(s3Secret.endpoint, s3Secret.region))
+            .withCredentials(AWSStaticCredentialsProvider(BasicAWSCredentials(s3Secret.accessKey, s3Secret.secretKey)))
             .build()
     }
 }
