c1c725259348f23e53bed6e671948b6100a5f940
kdomo <dongho5088@naver.com>
2023-05-30 19:53:49 +0900

    chore: S3Properties 생성과 환경변수 추가

---
+++ Whatnow-Infrastructure/src/main/kotlin/com/depromeet/whatnow/config/s3/S3Properties.kt
@@ -0,0 +1,17 @@
+package com.depromeet.whatnow.config.s3
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+
+@ConfigurationProperties(prefix = "ncp")
+@ConstructorBinding
+class S3Properties(
+    val s3: S3Secret
+) {
+    data class S3Secret(
+        val accessKey: String,
+        val secretKey: String,
+        val region: String,
+        val endpoint: String
+    )
+}
