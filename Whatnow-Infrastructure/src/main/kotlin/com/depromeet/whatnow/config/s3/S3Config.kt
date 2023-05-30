87743e1b61d779485683de64645a8e53cf485722
kdomo <dongho5088@naver.com>
2023-05-30 19:54:11 +0900

    chore: S3 Config 생성

---
+++ Whatnow-Infrastructure/src/main/kotlin/com/depromeet/whatnow/config/s3/S3Config.kt
@@ -0,0 +1,33 @@
+package com.depromeet.whatnow.config.s3
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.client.builder.AwsClientBuilder
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class S3Configs(
+    @Value("\${ncp.s3.access-key}")
+    val accessKey: String,
+
+    @Value("\${ncp.s3.secret-key}")
+    val secretKey: String,
+
+    @Value("\${ncp.s3.region}")
+    val region: String,
+
+    @Value("\${ncp.s3.endpoint}")
+    val endPoint: String
+) {
+    @Bean
+    fun amazonS3(): AmazonS3 {
+        return AmazonS3ClientBuilder.standard()
+            .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration(endPoint, region))
+            .withCredentials(AWSStaticCredentialsProvider(BasicAWSCredentials(accessKey, secretKey)))
+            .build()
+    }
+}
