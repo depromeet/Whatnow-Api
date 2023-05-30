b874c1cba9b9a8772dff9ff4fce0032581651331
kdomo <dongho5088@naver.com>
2023-05-30 20:05:50 +0900

    style: spotless

--- Whatnow-Infrastructure/src/main/kotlin/com/depromeet/whatnow/config/s3/ImageFileExtension.kt
+++ Whatnow-Infrastructure/src/main/kotlin/com/depromeet/whatnow/config/s3/ImageFileExtension.kt
@@ -3,7 +3,8 @@ package com.depromeet.whatnow.config.s3
 enum class ImageFileExtension(uploadExtension: String) {
     JPEG("jpeg"),
     JPG("jpg"),
-    PNG("png")
+    PNG("png"),
+    ;
 
-    val uploadExtension = uploadExtension,
+    val uploadExtension = uploadExtension
 }
