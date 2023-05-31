4861e56998e312c04d086b9f2613bf3d1268366c
이서현 <ymecca12@gmail.com>
2023-06-01 01:33:20 +0900

    refactor : H2config 삭제

--- Whatnow-Api/src/main/kotlin/com/depromeet/whatnow/api/promiseuser/controller/PromiseUserController.kt
+++ Whatnow-Api/src/main/kotlin/com/depromeet/whatnow/api/promiseuser/controller/PromiseUserController.kt
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/v1/promise-users")
+@RequestMapping("/v1/")
 @Tag(name = "4. [약속 유저]")
 @SecurityRequirement(name = "access-token")
 class PromiseUserController(
