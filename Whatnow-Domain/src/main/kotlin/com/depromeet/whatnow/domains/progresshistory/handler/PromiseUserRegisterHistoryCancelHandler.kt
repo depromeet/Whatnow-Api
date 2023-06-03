84081275e307849af1fb5a2f55180212efec7f35
이찬진 <water0641@naver.com>
2023-06-03 16:18:54 +0900

    feat : handler custom annotaion 추가

--- Whatnow-Domain/src/main/kotlin/com/depromeet/whatnow/domains/progresshistory/handler/PromiseUserRegisterHistoryCancelHandler.kt
+++ Whatnow-Domain/src/main/kotlin/com/depromeet/whatnow/domains/progresshistory/handler/PromiseUserRegisterHistoryCancelHandler.kt
@@ -1,12 +1,13 @@
 package com.depromeet.whatnow.domains.progresshistory.handler
 
+import com.depromeet.whatnow.annotation.Handler
 import com.depromeet.whatnow.events.domainEvent.PromiseUserCancelEvent
 import com.depromeet.whatnow.events.domainEvent.PromiseUserRegisterEvent
 import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
 
 
-
+@Handler
 class PromiseUserRegisterHistoryCancelHandler {
 
     @TransactionalEventListener(classes = [PromiseUserCancelEvent::class], phase = TransactionPhase.AFTER_COMMIT)
--- Whatnow-Domain/src/main/kotlin/com/depromeet/whatnow/domains/progresshistory/handler/PromiseUserRegisterHistoryInitHandler.kt
+++ Whatnow-Domain/src/main/kotlin/com/depromeet/whatnow/domains/progresshistory/handler/PromiseUserRegisterHistoryInitHandler.kt
@@ -1,11 +1,12 @@
 package com.depromeet.whatnow.domains.progresshistory.handler
 
+import com.depromeet.whatnow.annotation.Handler
 import com.depromeet.whatnow.events.domainEvent.PromiseUserRegisterEvent
 import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
 
 
-
+@Handler
 class PromiseUserRegisterHistoryInitHandler {
 
     @TransactionalEventListener(classes = [PromiseUserRegisterEvent::class], phase = TransactionPhase.AFTER_COMMIT)
