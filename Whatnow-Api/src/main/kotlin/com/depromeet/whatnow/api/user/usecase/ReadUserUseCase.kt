b9dd12d33c786ec12fd48043cb6ad3769f149cc6
이찬진 <water0641@naver.com>
2023-06-11 23:33:47 +0900

    refactor : readUserusecase query user 사용

--- Whatnow-Api/src/main/kotlin/com/depromeet/whatnow/api/user/usecase/ReadUserUseCase.kt
+++ Whatnow-Api/src/main/kotlin/com/depromeet/whatnow/api/user/usecase/ReadUserUseCase.kt
@@ -2,16 +2,15 @@ package com.depromeet.whatnow.api.user.usecase
 
 import com.depromeet.whatnow.annotation.UseCase
 import com.depromeet.whatnow.config.security.SecurityUtils
+import com.depromeet.whatnow.domains.user.adapter.UserAdapter
 import com.depromeet.whatnow.domains.user.domain.User
-import com.depromeet.whatnow.domains.user.repository.UserRepository
-import org.springframework.data.repository.findByIdOrNull
 
 @UseCase
 class ReadUserUseCase(
-    val userRepository: UserRepository,
+    val userAdapter: UserAdapter,
 ) {
     fun findUser(): User {
         val currentUserId: Long = SecurityUtils.currentUserId
-        return userRepository.findByIdOrNull(currentUserId)!!
+        return userAdapter.queryUser(currentUserId)
     }
 }
