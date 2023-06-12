102c43c6810eeb8f18d180b2a52cbe41bf80ed12
kdomo <dongho5088@naver.com>
2023-06-12 20:03:10 +0900

    test: S3UploadPresignedUrl Service Test

--- Whatnow-Infrastructure/src/test/kotlin/com/depromeet/whatnow/config/s3/S3UploadPresignedUrlServiceTest.kt
+++ Whatnow-Infrastructure/src/test/kotlin/com/depromeet/whatnow/config/s3/S3UploadPresignedUrlServiceTest.kt
@@ -1,5 +1,59 @@
 package com.depromeet.whatnow.config.s3
 
-import org.junit.jupiter.api.Assertions.*
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import com.amazonaws.client.builder.AwsClientBuilder
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.junit.jupiter.MockitoExtension
+import kotlin.test.assertContains
 
-class S3UploadPresignedUrlServiceTest
+@ExtendWith(MockitoExtension::class)
+class S3UploadPresignedUrlServiceTest {
+    lateinit var s3UploadPresignedUrlService: S3UploadPresignedUrlService
+
+    var s3Properties: S3Properties = S3Properties(
+        S3Properties.S3Secret(
+            accessKey = "accessKey",
+            secretKey = "secretKey",
+            region = "ap-northeast-2",
+            endpoint = "https://kr.object.ncloudstorage.com",
+            bucket = "bucket",
+        ),
+    )
+
+    var amazonS3: AmazonS3 =
+        AmazonS3ClientBuilder.standard()
+            .withEndpointConfiguration(AwsClientBuilder.EndpointConfiguration(s3Properties.s3.endpoint, s3Properties.s3.region))
+            .withCredentials(AWSStaticCredentialsProvider(BasicAWSCredentials(s3Properties.s3.accessKey, s3Properties.s3.secretKey)))
+            .build()
+
+    @BeforeEach
+    fun setup() {
+        s3UploadPresignedUrlService = S3UploadPresignedUrlService(amazonS3, s3Properties)
+    }
+
+    @Test
+    fun `PresignedUrl을 생성한다`() {
+        // given
+        val promiseId = 1L
+        val imageFIleExtension = ImageFileExtension.JPG
+
+        // when
+        val presignedUrl = s3UploadPresignedUrlService.forPromise(promiseId, imageFIleExtension)
+
+        // then
+        val resultUrl = """
+            https:
+            //${s3Properties.s3.bucket}.${s3Properties.s3.endpoint.replace("https://", "")}
+            /promise
+            /$promiseId
+            /${presignedUrl.key}.${imageFIleExtension.uploadExtension}
+            """
+
+        assertContains(presignedUrl.url, resultUrl)
+    }
+}
