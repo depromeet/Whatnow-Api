50b96c8aff84a9ff4e7327793dce1535c3574053
kdomo <dongho5088@naver.com>
2023-06-12 20:03:25 +0900

    test: GetPresignedUrl UseCase Test

--- Whatnow-Api/src/test/kotlin/com/depromeet/whatnow/api/image/usecase/GetPresignedUrlUseCaseTest.kt
+++ Whatnow-Api/src/test/kotlin/com/depromeet/whatnow/api/image/usecase/GetPresignedUrlUseCaseTest.kt
@@ -1,5 +1,43 @@
 package com.depromeet.whatnow.api.image.usecase
 
-import org.junit.jupiter.api.Assertions.*
+import com.depromeet.whatnow.config.s3.ImageFileExtension
+import com.depromeet.whatnow.config.s3.ImageUrlDto
+import com.depromeet.whatnow.config.s3.S3UploadPresignedUrlService
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.given
 
-class GetPresignedUrlUseCaseTest
+@ExtendWith(MockitoExtension::class)
+class GetPresignedUrlUseCaseTest {
+    @Mock
+    lateinit var presignedUrlService: S3UploadPresignedUrlService
+
+    @InjectMocks
+    lateinit var getPresignedUrlUseCase: GetPresignedUrlUseCase
+
+    @Test
+    fun `PresignUrl 을 요청하면 url 을 반환한다`() {
+        // given
+        ImageUrlDto(
+            url = "https://whatnow.kr/1.jpg",
+            key = "1.jpg",
+        )
+
+        given(presignedUrlService.forPromise(1, ImageFileExtension.JPEG)).willReturn(
+            ImageUrlDto(
+                url = "https://whatnow.kr/1.jpg",
+                key = "1.jpg",
+            ),
+        )
+        // when
+        val imageUrlResponse = getPresignedUrlUseCase.forPromise(1, ImageFileExtension.JPEG)
+
+        // then
+        assertEquals("https://whatnow.kr/1.jpg", imageUrlResponse.presignedUrl)
+        assertEquals("1.jpg", imageUrlResponse.key)
+    }
+}
