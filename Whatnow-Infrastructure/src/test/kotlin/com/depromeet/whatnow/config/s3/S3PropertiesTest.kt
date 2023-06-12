943269908e209fc3315ec4570db7485892f41e1e
kdomo <dongho5088@naver.com>
2023-06-12 20:02:48 +0900

    test: S3Properties Test

--- Whatnow-Infrastructure/src/test/kotlin/com/depromeet/whatnow/config/s3/S3PropertiesTest.kt
+++ Whatnow-Infrastructure/src/test/kotlin/com/depromeet/whatnow/config/s3/S3PropertiesTest.kt
@@ -1,5 +1,17 @@
 package com.depromeet.whatnow.config.s3
 
-import org.junit.jupiter.api.Assertions.*
+import com.depromeet.whatnow.config.InfraIntegrateSpringBootTest
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 
-class S3PropertiesTest
+@InfraIntegrateSpringBootTest
+class S3PropertiesTest {
+    @Autowired
+    lateinit var s3Properties: S3Properties
+
+    @Test
+    fun `s3 프로퍼티가 제대로 init 되어야한다`() {
+        assertEquals(s3Properties.s3.endpoint, "https://kr.object.ncloudstorage.com")
+    }
+}
