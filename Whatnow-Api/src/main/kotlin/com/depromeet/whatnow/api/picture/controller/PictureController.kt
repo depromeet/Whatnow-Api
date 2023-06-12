c2c14e3f72b9796aefa3aa913bc33e1827d65a31
도모 <dongho5088@naver.com>
2023-06-12 17:27:42 +0900

    [DPMBE-61] 이미지 성공 확인 시 late, wait에 따라 각각 다른 메세지를 보내도록 만든다. (#73)
    * feat: 이미지 성공 확인 시 late, wait에 따라 각각 다른 메세지를 보내도록 만든다.
    
    * feat: Picture 에러코드 나열
    
    * refactor: PictureRegisterEventHandler에 Hanlder 어노테이션으로 변경
    
    * style: spotless
    
    * refactor: PictureCommentType에 promiseUserType추가하여 Service 코드 개선
    
    * test: 비지니스 로적 수정에 따른 PictureRegisterEventHandler Test 수정
    
    * style: 주석제거

--- Whatnow-Api/src/main/kotlin/com/depromeet/whatnow/api/picture/controller/PictureController.kt
+++ Whatnow-Api/src/main/kotlin/com/depromeet/whatnow/api/picture/controller/PictureController.kt
@@ -1,12 +1,14 @@
 package com.depromeet.whatnow.api.picture.controller
 
 import com.depromeet.whatnow.api.picture.usecase.PictureUploadSuccessUseCase
+import com.depromeet.whatnow.domains.picture.domain.PictureCommentType
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -19,7 +21,7 @@ class PictureController(
 
     @Operation(summary = "약속 관련 이미지 업로드 성공 요청")
     @PostMapping("/promises/{promiseId}/images/success/{imageKey}")
-    fun successUploadImage(@PathVariable promiseId: Long, @PathVariable imageKey: String) {
-        successUseCase.successUploadImage(promiseId, imageKey)
+    fun successUploadImage(@PathVariable promiseId: Long, @PathVariable imageKey: String, @RequestParam pictureCommentType: PictureCommentType) {
+        successUseCase.successUploadImage(promiseId, imageKey, pictureCommentType)
     }
 }
