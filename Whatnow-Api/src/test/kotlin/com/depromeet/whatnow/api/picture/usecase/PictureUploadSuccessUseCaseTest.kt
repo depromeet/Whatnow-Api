37ae1e8599ce55f686406ee35f9b9e215db4d42c
kdomo <dongho5088@naver.com>
2023-06-12 23:46:07 +0900

    test: PictureUploadSuccess UseCase Test

---
+++ Whatnow-Api/src/test/kotlin/com/depromeet/whatnow/api/picture/usecase/PictureUploadSuccessUseCaseTest.kt
@@ -0,0 +1,50 @@
+package com.depromeet.whatnow.api.picture.usecase
+
+import com.depromeet.whatnow.config.security.SecurityUtils
+import com.depromeet.whatnow.domains.picture.domain.PictureCommentType
+import com.depromeet.whatnow.domains.picture.service.PictureDomainService
+import org.assertj.core.api.Assertions.assertThatCode
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.BDDMockito.given
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.any
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.test.context.support.WithMockUser
+
+@ExtendWith(MockitoExtension::class)
+class PictureUploadSuccessUseCaseTest {
+    @Mock
+    lateinit var pictureDomainService: PictureDomainService
+
+
+    @InjectMocks
+    lateinit var pictureUploadSuccessUseCase: PictureUploadSuccessUseCase
+
+    @BeforeEach
+    fun setup() {
+        val securityContext = SecurityContextHolder.createEmptyContext()
+        val authentication = UsernamePasswordAuthenticationToken("1", null, setOf(SimpleGrantedAuthority("ROLE_USER")))
+        securityContext.authentication = authentication
+        SecurityContextHolder.setContext(securityContext)
+    }
+
+    @Test
+    fun `이미지 업로드 성공 요청시 정상적이라면 에러가 발생하지 않는다`() {
+        // given
+
+        // when
+
+        // then
+        assertThatCode {
+            pictureUploadSuccessUseCase.successUploadImage(1, "1", PictureCommentType.SORRY_LATE)
+        }.doesNotThrowAnyException()
+    }
+}
