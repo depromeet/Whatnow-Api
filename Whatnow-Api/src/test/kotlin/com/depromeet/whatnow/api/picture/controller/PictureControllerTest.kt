c167789e5c1cda9951384e338a869c54c7b5c946
kdomo <dongho5088@naver.com>
2023-06-13 23:02:09 +0900

    test: Picture Controller Test

---
+++ Whatnow-Api/src/test/kotlin/com/depromeet/whatnow/api/picture/controller/PictureControllerTest.kt
@@ -0,0 +1,44 @@
+package com.depromeet.whatnow.api.picture.controller
+
+import com.depromeet.whatnow.api.picture.usecase.PictureUploadSuccessUseCase
+import com.depromeet.whatnow.domains.picture.domain.PictureCommentType
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import org.springframework.security.test.context.support.WithMockUser
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.user
+import org.springframework.test.context.ContextConfiguration
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
+
+@WebMvcTest(PictureController::class)
+@ContextConfiguration(classes = [PictureController::class])
+@AutoConfigureMockMvc
+class PictureControllerTest {
+    @MockBean
+    lateinit var successUseCase: PictureUploadSuccessUseCase
+
+    @Autowired
+    lateinit var mockMvc: MockMvc
+
+    @Test
+    @WithMockUser(username = "test", roles = ["USER"])
+    fun `이미지 업로드 성공 요청에 정상적으로 200을 반환한다`() {
+        // given
+        val promiseId = 1
+        val imageKey = "imageKey"
+        val pictureCommentType = PictureCommentType.SORRY_LATE
+
+        // when, then
+        mockMvc.perform(
+            post("/v1/promises/{promiseId}/images/success/{imageKey}", promiseId, imageKey)
+                .param("pictureCommentType", pictureCommentType.name)
+                .with(user("test").roles("USER")),
+        )
+            .andExpect(status().isOk)
+            .andDo { print(it) }
+    }
+}
