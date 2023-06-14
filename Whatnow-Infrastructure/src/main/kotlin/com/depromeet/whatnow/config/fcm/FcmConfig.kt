746981a644e62212a87f28d4f592b6d2e2836963
도모 <dongho5088@naver.com>
2023-06-14 14:14:46 +0900

    [DPMBE-64] FCM 기능을 추가한다 (#81)
    * chore: firebase-admin 의존성 추가
    
    * feat: FcmConfig 추가
    
    * chore: fcm certification 환경변수 추가
    
    * feat: Fcm Service 추가
    
    * style: spotless
    
    * fix: test시 환경변수 없어도 돌아가도록 수정

---
+++ Whatnow-Infrastructure/src/main/kotlin/com/depromeet/whatnow/config/fcm/FcmConfig.kt
@@ -0,0 +1,36 @@
+package com.depromeet.whatnow.config.fcm
+
+import com.google.auth.oauth2.GoogleCredentials
+import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+import java.io.ByteArrayInputStream
+import java.nio.charset.StandardCharsets
+import javax.annotation.PostConstruct
+
+@Configuration
+class FcmConfig(
+    @Value("\${fcm.certification}")
+    private val fcmCertification: String,
+) {
+    @PostConstruct
+    fun init() {
+        try {
+            if (FirebaseApp.getApps().isEmpty()) {
+                val options = FirebaseOptions.builder()
+                    .setCredentials(
+                        GoogleCredentials.fromStream(
+                            ByteArrayInputStream(
+                                fcmCertification.toByteArray(StandardCharsets.UTF_8),
+                            ),
+                        ),
+                    )
+                    .build()
+                FirebaseApp.initializeApp(options)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+}
