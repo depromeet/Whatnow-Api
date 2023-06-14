3f509dd3c0dee5931b7a977884ed5a5643cf06a1
도모 <dongho5088@naver.com>
2023-06-14 14:14:46 +0900

    [DPMBE-64] FCM 기능을 추가한다 (#81)
    * chore: firebase-admin 의존성 추가
    
    * feat: FcmConfig 추가
    
    * chore: fcm certification 환경변수 추가
    
    * feat: Fcm Service 추가
    
    * style: spotless
    
    * fix: test시 환경변수 없어도 돌아가도록 수정

---
+++ Whatnow-Infrastructure/src/main/kotlin/com/depromeet/whatnow/config/fcm/FcmService.kt
@@ -0,0 +1,38 @@
+package com.depromeet.whatnow.config.fcm
+
+import com.google.api.core.ApiFuture
+import com.google.firebase.messaging.BatchResponse
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.firebase.messaging.Message
+import com.google.firebase.messaging.MulticastMessage
+import com.google.firebase.messaging.Notification
+import org.springframework.stereotype.Service
+
+@Service
+class FcmService {
+    fun sendGroupMessageAsync(
+        tokenList: List<String>,
+        title: String,
+        content: String,
+        imageUrl: String,
+    ): ApiFuture<BatchResponse> {
+        val multicast = MulticastMessage.builder()
+            .addAllTokens(tokenList)
+            .setNotification(
+                Notification.builder()
+                    .setTitle(title)
+                    .setBody(content)
+                    .setImage(imageUrl)
+                    .build(),
+            ).build()
+        return FirebaseMessaging.getInstance().sendMulticastAsync(multicast)
+    }
+
+    fun sendMessageSync(token: String, content: String) {
+        val message = Message.builder()
+            .setToken(token)
+            .setNotification(Notification.builder().setBody(content).build())
+            .build()
+        FirebaseMessaging.getInstance().send(message)
+    }
+}
