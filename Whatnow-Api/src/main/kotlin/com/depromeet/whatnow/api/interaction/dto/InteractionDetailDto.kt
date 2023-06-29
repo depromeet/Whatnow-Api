c2a9051e4abbed828a980ece02ded1ea0f3f7d58
kdomo <dongho5088@naver.com>
2023-06-29 21:34:39 +0900

    style: spotless

--- Whatnow-Api/src/main/kotlin/com/depromeet/whatnow/api/interaction/dto/InteractionDetailDto.kt
+++ Whatnow-Api/src/main/kotlin/com/depromeet/whatnow/api/interaction/dto/InteractionDetailDto.kt
@@ -2,7 +2,6 @@ package com.depromeet.whatnow.api.interaction.dto
 
 import com.depromeet.whatnow.common.vo.UserInfoVo
 import com.depromeet.whatnow.domains.interaction.domain.InteractionType
-import com.depromeet.whatnow.domains.interactionhistory.domain.InteractionHistory
 
 data class InteractionDetailDto(
     val senderUser: UserInfoVo,
