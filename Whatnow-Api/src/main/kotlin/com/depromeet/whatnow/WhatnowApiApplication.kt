f23faef21dda0059221fa38cb872ac3cb839aa32
이서현 <ymecca12@gmail.com>
2023-06-30 16:20:25 +0900

    feat: 행정동 조히

--- Whatnow-Api/src/main/kotlin/com/depromeet/whatnow/WhatnowApiApplication.kt
+++ Whatnow-Api/src/main/kotlin/com/depromeet/whatnow/WhatnowApiApplication.kt
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = ["com.depromeet.whatnow.domains.district"])
+@EnableMongoRepositories(basePackages = ["com.depromeet.whatnow.domains.district"])
 class WhatnowApiApplication
 
 fun main(args: Array<String>) {
--- Whatnow-Domain/src/main/kotlin/com/depromeet/whatnow/domains/district/repository/DistrictRepository.kt
+++ Whatnow-Domain/src/main/kotlin/com/depromeet/whatnow/domains/district/repository/DistrictRepository.kt
@@ -1,8 +1,13 @@
 package com.depromeet.whatnow.domains.district.repository
 
 import com.depromeet.whatnow.domains.district.domain.District
+import com.mongodb.client.model.geojson.Point
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.mongodb.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
-interface DistrictRepository: MongoRepository<District, String>
+interface DistrictRepository : MongoRepository<District, String>{
+    @Query("{\$geoIntersects: {\$geometry: {type: 'Point', coordinates: ?0}}}")
+    fun findByLocationIntersects(coordinates: Point?): District
+}
