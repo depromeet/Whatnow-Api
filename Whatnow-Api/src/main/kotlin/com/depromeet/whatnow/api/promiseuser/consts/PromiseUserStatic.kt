136372869426a93d5541f0b5b5f06c2bda99d293
이서현 <ymecca12@gmail.com>
2023-07-01 17:42:14 +0900

    chore : spotless

--- Whatnow-Api/src/main/kotlin/com/depromeet/whatnow/api/promiseuser/consts/PromiseUserStatic.kt
+++ Whatnow-Api/src/main/kotlin/com/depromeet/whatnow/api/promiseuser/consts/PromiseUserStatic.kt
@@ -1,6 +1,5 @@
 package com.depromeet.whatnow.api.promiseuser.consts
 
-
 const val PROMISE_USER_NOT_FOUND = "해당 약속에 참여한 유저가 아닙니다."
 const val PROMISE_USER_REGISTER_SUCCESS_MESSAGE = "약속 참여가 완료되었습니다."
 const val PROMISE_USER_REGISTER_FAIL_MESSAGE = "약속 참여에 실패하였습니다."
