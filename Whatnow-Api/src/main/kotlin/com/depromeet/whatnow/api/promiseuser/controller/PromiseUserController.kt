c0a3663e787d123a1202e0b2b92846f2fb24540e
이서현 <ymecca12@gmail.com>
2023-07-02 21:23:19 +0900

    refactor : post, put, delete 반환형 수정

--- Whatnow-Api/src/main/kotlin/com/depromeet/whatnow/api/promiseuser/controller/PromiseUserController.kt
+++ Whatnow-Api/src/main/kotlin/com/depromeet/whatnow/api/promiseuser/controller/PromiseUserController.kt
@@ -1,7 +1,5 @@
 package com.depromeet.whatnow.api.promiseuser.controller
 
-import com.depromeet.whatnow.api.promiseuser.consts.PROMISE_USER_DELETE_SUCCESS_MESSAGE
-import com.depromeet.whatnow.api.promiseuser.consts.PROMISE_USER_REGISTER_SUCCESS_MESSAGE
 import com.depromeet.whatnow.api.promiseuser.dto.PromiseUserDto
 import com.depromeet.whatnow.api.promiseuser.usecase.PromiseUserReadUseCase
 import com.depromeet.whatnow.api.promiseuser.usecase.PromiseUserRecordUseCase
@@ -10,8 +8,6 @@ import com.depromeet.whatnow.domains.promiseuser.domain.PromiseUserType
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.http.HttpStatus
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -20,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/v1/")
+@RequestMapping("/v1")
 @Tag(name = "4. [약속 유저]")
 @SecurityRequirement(name = "access-token")
 class PromiseUserController(
@@ -28,28 +24,26 @@ class PromiseUserController(
     val promiseUserReadUseCase: PromiseUserReadUseCase,
 ) {
     @Operation(summary = "약속 유저(promise) 생성", description = "약속에 유저가 참여합니다.")
-    @PostMapping("promises/{promise-id}/users")
-    fun joinPromise(@PathVariable("promise-id") promiseId: Long, userId: Long, userLocation: CoordinateVo): ResponseEntity<String> {
-        promiseUserRecordUseCase.createPromiseUser(promiseId, userId, userLocation)
-        return ResponseEntity(PROMISE_USER_REGISTER_SUCCESS_MESSAGE, HttpStatus.OK)
+    @PostMapping("/promises/{promise-id}/users")
+    fun joinPromise(@PathVariable("promise-id") promiseId: Long, userId: Long, userLocation: CoordinateVo): PromiseUserDto {
+        return promiseUserRecordUseCase.createPromiseUser(promiseId, userId, userLocation)
     }
 
     @Operation(summary = "약속 id로 약속 유저(promiseUser) 조회", description = "약속ID 로 약속 유저를 조회합니다.")
-    @GetMapping("promises/{promiseId}/users")
+    @GetMapping("/promises/{promiseId}/users")
     fun getPromiseUser(@PathVariable("promise-id") promiseId: Long): List<PromiseUserDto> {
         return promiseUserReadUseCase.findByPromiseId(promiseId)
     }
 
     @Operation(summary = "약속id 와 유저 id 로 약속에 참여한 유저의 정보를 조회", description = "약속ID 와 유저ID 로 약속에 참여한 유저의 정보를 조회합니다.")
-    @GetMapping("promises/{promise-id}/users/{user-id}")
+    @GetMapping("/promises/{promise-id}/users/{user-id}")
     fun getPromiseUserByPromiseUserType(@PathVariable("promise-id") promiseId: Long, @PathVariable("user-id") userId: Long): PromiseUserDto {
         return promiseUserReadUseCase.findPromiseUserByPromiseIdAndUserId(promiseId, userId)
     }
 
     @Operation(summary = "유저가 약속을 취소(상태로 변경)", description = "userId로 참여한 약속 유저를 취소(cancel) 합니다.")
-    @PutMapping("promises/{promise-id}/users/{user-id}/status/{status}")
-    fun cancelPromise(@PathVariable("promise-id") promiseId: Long, @PathVariable("user-id") userId: Long, @PathVariable("status") promiseUserType: PromiseUserType): ResponseEntity<String> {
-        promiseUserRecordUseCase.updatePromiseUserType(promiseId, userId, promiseUserType)
-        return ResponseEntity(PROMISE_USER_DELETE_SUCCESS_MESSAGE, HttpStatus.OK)
+    @PutMapping("/promises/{promise-id}/users/{user-id}/status/{status}")
+    fun cancelPromise(@PathVariable("promise-id") promiseId: Long, @PathVariable("user-id") userId: Long, @PathVariable("status") promiseUserType: PromiseUserType): PromiseUserDto {
+        return promiseUserRecordUseCase.updatePromiseUserType(promiseId, userId, promiseUserType)
     }
 }
--- Whatnow-Api/src/main/kotlin/com/depromeet/whatnow/api/promiseuser/usecase/PromiseUserRecordUseCase.kt
+++ Whatnow-Api/src/main/kotlin/com/depromeet/whatnow/api/promiseuser/usecase/PromiseUserRecordUseCase.kt
@@ -1,7 +1,9 @@
 package com.depromeet.whatnow.api.promiseuser.usecase
 
 import com.depromeet.whatnow.annotation.UseCase
+import com.depromeet.whatnow.api.promiseuser.dto.PromiseUserDto
 import com.depromeet.whatnow.common.vo.CoordinateVo
+import com.depromeet.whatnow.domains.progresshistory.domain.PromiseProgress.DEFAULT
 import com.depromeet.whatnow.domains.promiseuser.domain.PromiseUser
 import com.depromeet.whatnow.domains.promiseuser.domain.PromiseUserType
 import com.depromeet.whatnow.domains.promiseuser.service.PromiseUserDomainService
@@ -10,13 +12,16 @@ import com.depromeet.whatnow.domains.promiseuser.service.PromiseUserDomainServic
 class PromiseUserRecordUseCase(
     val promiseUserDomainService: PromiseUserDomainService,
 ) {
-    fun createPromiseUser(promiseId: Long, userId: Long, userLocation: CoordinateVo) {
-        promiseUserDomainService.createPromiseUser(
-            PromiseUser(
-                promiseId = promiseId,
-                userId = userId,
-                userLocation = userLocation,
+    fun createPromiseUser(promiseId: Long, userId: Long, userLocation: CoordinateVo): PromiseUserDto {
+        return PromiseUserDto.of(
+            promiseUserDomainService.createPromiseUser(
+                PromiseUser(
+                    promiseId = promiseId,
+                    userId = userId,
+                    userLocation = userLocation,
+                ),
             ),
+            progress = DEFAULT,
         )
     }
 
@@ -24,7 +29,14 @@ class PromiseUserRecordUseCase(
         promiseUserDomainService.withDraw(promiseId, userId)
     }
 
-    fun updatePromiseUserType(promiseId: Long, userId: Long, promiseUserType: PromiseUserType) {
-        promiseUserDomainService.updatePromiseUserType(promiseId, userId, promiseUserType)
+    fun updatePromiseUserType(promiseId: Long, userId: Long, promiseUserType: PromiseUserType): PromiseUserDto {
+        return PromiseUserDto.of(
+            promiseUserDomainService.updatePromiseUserType(
+                promiseId = promiseId,
+                userId = userId,
+                promiseUserType = promiseUserType,
+            ),
+            progress = DEFAULT,
+        )
     }
 }
