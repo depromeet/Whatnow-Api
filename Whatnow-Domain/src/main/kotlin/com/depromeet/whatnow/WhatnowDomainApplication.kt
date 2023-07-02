75003ba88e76a0e1885dea7cdcb0b993dc9880ac
이서현 <ymecca12@gmail.com>
2023-07-02 20:20:09 +0900

    refactor : exclude mongoAutoConfig - application.yml exclude add

--- Whatnow-Domain/src/main/kotlin/com/depromeet/whatnow/WhatnowDomainApplication.kt
+++ Whatnow-Domain/src/main/kotlin/com/depromeet/whatnow/WhatnowDomainApplication.kt
@@ -1,12 +1,9 @@
 package com.depromeet.whatnow
 
-import com.depromeet.whatnow.config.mongo.MongoConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.Import
 
 @SpringBootApplication
-@Import(MongoConfig::class)
 class WhatnowDomainApplication
 
 fun main(args: Array<String>) {
