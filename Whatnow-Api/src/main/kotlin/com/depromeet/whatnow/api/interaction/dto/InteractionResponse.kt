bffb93aba09fcb7973109fd4d02bd220ecf4eaf6
도모 <dongho5088@naver.com>
2023-07-04 03:42:02 +0900

    feat: 인터렉션 조회 응답값에 Progress 값을 추가한다 (#127)

--- Whatnow-Api/src/main/kotlin/com/depromeet/whatnow/api/interaction/dto/InteractionResponse.kt
+++ Whatnow-Api/src/main/kotlin/com/depromeet/whatnow/api/interaction/dto/InteractionResponse.kt
@@ -1,5 +1,8 @@
 package com.depromeet.whatnow.api.interaction.dto
 
+import com.depromeet.whatnow.api.promiseprogress.dto.response.UserProgressResponse
+
 data class InteractionResponse(
+    val userProgressResponse: UserProgressResponse,
     val interactionDtoList: List<InteractionDto>,
 )
