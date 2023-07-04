9996250d1c1a5c2eabe8ea8493485597d9811dd7
kdomo <dongho5088@naver.com>
2023-07-04 22:51:53 +0900

    style: ASSERT_IMAGE_DOMAIN 상수 적용

--- Whatnow-Domain/src/main/kotlin/com/depromeet/whatnow/domains/progresshistory/domain/PromiseProgress.kt
+++ Whatnow-Domain/src/main/kotlin/com/depromeet/whatnow/domains/progresshistory/domain/PromiseProgress.kt
@@ -1,5 +1,6 @@
 package com.depromeet.whatnow.domains.progresshistory.domain
 
+import com.depromeet.whatnow.consts.ASSERT_IMAGE_DOMAIN
 import com.fasterxml.jackson.annotation.JsonFormat
 
 @JsonFormat(shape = JsonFormat.Shape.OBJECT)
@@ -9,17 +10,17 @@ enum class PromiseProgress(
     val code: String,
     val image: String,
 ) {
-    SHOWER(PromiseProgressGroup.PREPARING, "씻는 중", "SHOWER", "https://image.whatnow.kr/assert/progresses/SHOWER.svg"),
-    LEAVE_SOON(PromiseProgressGroup.PREPARING, "곧 나가", "LEAVE_SOON", "https://image.whatnow.kr/assert/progresses/LEAVE_SOON.svg"),
-    BED(PromiseProgressGroup.PREPARING, "아직 침대", "BED", "https://image.whatnow.kr/assert/progresses/BED.svg"),
-    WALKING(PromiseProgressGroup.MOVING, "걸어가는 중", "WALKING", "https://image.whatnow.kr/assert/progresses/WALKING.svg"),
-    RUNNING(PromiseProgressGroup.MOVING, "뛰고 있어", "RUNNING", "https://image.whatnow.kr/assert/progresses/RUNNING.svg"),
-    BOARDING(PromiseProgressGroup.MOVING, "탑승 중", "BOARDING", "https://image.whatnow.kr/assert/progresses/BOARDING.svg"),
-    SORRY(PromiseProgressGroup.LATE, "미안해", "SORRY", "https://image.whatnow.kr/assert/progresses/SORRY.svg"),
-    TEAR(PromiseProgressGroup.LATE, "눈물 줄줄", "TEAR", "https://image.whatnow.kr/assert/progresses/TEAR.svg"),
-    URGENCY(PromiseProgressGroup.LATE, "급하다!!", "URGENCY", "https://image.whatnow.kr/assert/progresses/URGENCY.svg"),
-    AFTER_SOON(PromiseProgressGroup.EXPECTED_TIME, "곧 도착", "AFTER_SOON", "https://image.whatnow.kr/assert/progresses/AFTER_SOON.svg"),
-    AFTER_5(PromiseProgressGroup.EXPECTED_TIME, "5분 뒤 도착", "AFTER_5", "https://image.whatnow.kr/assert/progresses/AFTER_5.svg"),
-    AFTER_10(PromiseProgressGroup.EXPECTED_TIME, "10분 뒤 도착", "AFTER_10", "https://image.whatnow.kr/assert/progresses/AFTER_10.svg"),
-    DEFAULT(PromiseProgressGroup.DEFAULT, "기본", "DEFAULT", "https://image.whatnow.kr/assert/progresses/DEFAULT.svg"),
+    SHOWER(PromiseProgressGroup.PREPARING, "씻는 중", "SHOWER", "$ASSERT_IMAGE_DOMAIN/progresses/SHOWER.svg"),
+    LEAVE_SOON(PromiseProgressGroup.PREPARING, "곧 나가", "LEAVE_SOON", "$ASSERT_IMAGE_DOMAIN/progresses/LEAVE_SOON.svg"),
+    BED(PromiseProgressGroup.PREPARING, "아직 침대", "BED", "$ASSERT_IMAGE_DOMAIN/progresses/BED.svg"),
+    WALKING(PromiseProgressGroup.MOVING, "걸어가는 중", "WALKING", "$ASSERT_IMAGE_DOMAIN/progresses/WALKING.svg"),
+    RUNNING(PromiseProgressGroup.MOVING, "뛰고 있어", "RUNNING", "$ASSERT_IMAGE_DOMAIN/progresses/RUNNING.svg"),
+    BOARDING(PromiseProgressGroup.MOVING, "탑승 중", "BOARDING", "$ASSERT_IMAGE_DOMAIN/progresses/BOARDING.svg"),
+    SORRY(PromiseProgressGroup.LATE, "미안해", "SORRY", "$ASSERT_IMAGE_DOMAIN/progresses/SORRY.svg"),
+    TEAR(PromiseProgressGroup.LATE, "눈물 줄줄", "TEAR", "$ASSERT_IMAGE_DOMAIN/progresses/TEAR.svg"),
+    URGENCY(PromiseProgressGroup.LATE, "급하다!!", "URGENCY", "$ASSERT_IMAGE_DOMAIN/progresses/URGENCY.svg"),
+    AFTER_SOON(PromiseProgressGroup.EXPECTED_TIME, "곧 도착", "AFTER_SOON", "$ASSERT_IMAGE_DOMAIN/progresses/AFTER_SOON.svg"),
+    AFTER_5(PromiseProgressGroup.EXPECTED_TIME, "5분 뒤 도착", "AFTER_5", "$ASSERT_IMAGE_DOMAIN/progresses/AFTER_5.svg"),
+    AFTER_10(PromiseProgressGroup.EXPECTED_TIME, "10분 뒤 도착", "AFTER_10", "$ASSERT_IMAGE_DOMAIN/progresses/AFTER_10.svg"),
+    DEFAULT(PromiseProgressGroup.DEFAULT, "기본", "DEFAULT", "$ASSERT_IMAGE_DOMAIN/progresses/DEFAULT.svg"),
 }
