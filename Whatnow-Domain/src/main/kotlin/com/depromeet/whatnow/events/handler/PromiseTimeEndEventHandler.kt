2aa42b657f2ceb9fa1aed0f49ab83b97c31400c3
이서현 <ymecca12@gmail.com>
2023-07-05 21:47:52 +0900

    feat : 거리 측정 및 Promise 시작, 종료시간 Event Handler

--- Whatnow-Domain/src/main/kotlin/com/depromeet/whatnow/events/handler/PromiseTimeEndEventHandler.kt
+++ Whatnow-Domain/src/main/kotlin/com/depromeet/whatnow/events/handler/PromiseTimeEndEventHandler.kt
@@ -1,18 +1,52 @@
 package com.depromeet.whatnow.events.handler
 
+import com.depromeet.whatnow.domains.promise.adaptor.PromiseAdaptor
+import com.depromeet.whatnow.domains.promiseuser.domain.PromiseUserType.CANCEL
+import com.depromeet.whatnow.domains.promiseuser.service.PromiseUserDomainService
+import com.depromeet.whatnow.events.domainEvent.MeetPromiseUserEvent
 import com.depromeet.whatnow.events.domainEvent.PromiseTimeEndEvent
-import com.depromeet.whatnow.events.domainEvent.PromiseTimeStartEvent
+import com.depromeet.whatnow.events.domainEvent.PromiseUpdateEndTimeEvent
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
 
 @Component
-class PromiseTimeEndEventHandler {
-
+class PromiseTimeEndEventHandler(
+    val promiseAdaptor: PromiseAdaptor,
+    val promiseUserDomainService: PromiseUserDomainService,
+) {
     @Async
     @TransactionalEventListener(classes = [PromiseTimeEndEvent::class], phase = TransactionPhase.AFTER_COMMIT)
-    fun handleRegisterUserEvent(promiseTimeStartEvent: PromiseTimeStartEvent) {
-        val promiseId = promiseTimeStartEvent.promiseId
+    fun handleRegisterUserEvent(endTimePromiseEvent: PromiseUpdateEndTimeEvent) {
+        val promiseId: Long = endTimePromiseEvent.promiseId
+        val promise = promiseAdaptor.queryPromise(promiseId)
+        val coordinate = promise.meetPlace?.coordinate
+        val promiseUsers = promiseUserDomainService.findByPromiseId(promiseId)
+        for (promiseUser in promiseUsers) {
+            when (promiseUser.promiseUserType) {
+                // 만약 유저가 약속을 취소한 상태라면 update 하지 않는다.
+                CANCEL -> continue
+
+                else -> {
+                    // 만약 유저가 위치가 약속 장소 도착이면 wait 로 상태 변경
+                    if (promiseUserDomainService.isArrived(promiseUser, coordinate!!)) {
+                        promiseUser.updatePromiseUserTypeToWait()
+                    }
+                    // 만약 유저가 위치가 약속 장소 도착이 아니면 late 로 상태 변경
+                    else {
+                        promiseUser.updatePromiseUserTypeToLate()
+                    }
+                }
+            }
+        }
+    }
+
+    @Async
+    @TransactionalEventListener(classes = [MeetPromiseUserEvent::class], phase = TransactionPhase.AFTER_COMMIT)
+    fun handleMeetPromiseUserEvent(meetPromiseUserEvent: MeetPromiseUserEvent) {
+//        만났을 경우
+//        for(promiseUser in promiseUsers){
+//        }
     }
 }
--- Whatnow-Domain/src/main/kotlin/com/depromeet/whatnow/events/handler/PromiseTimeStartEventHandler.kt
+++ Whatnow-Domain/src/main/kotlin/com/depromeet/whatnow/events/handler/PromiseTimeStartEventHandler.kt
@@ -1,5 +1,6 @@
 package com.depromeet.whatnow.events.handler
 
+import com.depromeet.whatnow.domains.promiseuser.adaptor.PromiseUserAdaptor
 import com.depromeet.whatnow.events.domainEvent.PromiseTimeStartEvent
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
@@ -7,11 +8,18 @@ import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
 
 @Component
-class PromiseTimeStartEventHandler {
-
+class PromiseTimeStartEventHandler(
+    val promiseUserAdaptor: PromiseUserAdaptor,
+) {
     @Async
     @TransactionalEventListener(classes = [PromiseTimeStartEvent::class], phase = TransactionPhase.AFTER_COMMIT)
     fun handleRegisterUserEvent(promiseTimeStartEvent: PromiseTimeStartEvent) {
         val promiseId = promiseTimeStartEvent.promiseId
+        val promiseUsers = promiseUserAdaptor.findByPromiseId(promiseId)
+
+        promiseUsers.forEach { promiseUser ->
+            promiseUser.updatePromiseUserTypeToWait()
+            promiseUser.userLocationInit()
+        }
     }
 }
