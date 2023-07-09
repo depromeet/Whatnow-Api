b955fcb3902f24048641db0a331059ab49d707db
이서현 <ymecca12@gmail.com>
2023-07-09 19:23:07 +0900

    feat : slack sender 전송 방식으로 변경

--- Whatnow-Infrastructure/src/main/kotlin/com/depromeet/whatnow/api/config/slack/SlackErrorNotificationProvider.kt
+++ Whatnow-Infrastructure/src/main/kotlin/com/depromeet/whatnow/api/config/slack/SlackErrorNotificationProvider.kt
@@ -3,6 +3,7 @@ package com.depromeet.whatnow.api.config.slack
 import com.depromeet.whatnow.annotation.Helper
 import com.depromeet.whatnow.config.slack.SlackProperties
 import com.depromeet.whatnow.config.slack.SlackProperties.SlackSecret
+import com.depromeet.whatnow.consts.SLACK_MAX_LENGTH
 import com.slack.api.model.block.LayoutBlock
 import org.springframework.scheduling.annotation.Async
 import java.util.Arrays
@@ -13,16 +14,15 @@ class SlackErrorNotificationProvider(
     val slackProperties: SlackProperties,
 ) {
     var slackWebHook: SlackSecret = slackProperties.webhook
-    val MAX_LENGTH = 500
 
     fun getErrorStack(throwable: Throwable): String {
         val exceptionAsStrings = Arrays.toString(throwable.stackTrace)
-        val cutLength = Math.min(exceptionAsStrings.length, MAX_LENGTH)
+        val cutLength = Math.min(exceptionAsStrings.length, SLACK_MAX_LENGTH)
         return exceptionAsStrings.substring(0, cutLength)
     }
 
     @Async
     fun sendNotification(layoutBlocks: List<LayoutBlock>) {
-        slackHelper.sendNotification(slackWebHook.channelId, layoutBlocks)
+        slackHelper.sendNotification(slackWebHook.url, slackWebHook.token, slackWebHook.channelId, layoutBlocks)
     }
 }
