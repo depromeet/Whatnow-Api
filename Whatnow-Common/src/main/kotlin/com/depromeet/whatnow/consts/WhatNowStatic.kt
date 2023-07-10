8158ad534d8ca3ae37894ea1332a015ecb4484cc
BlackBean99 <54030889+BlackBean99@users.noreply.github.com>
2023-07-10 15:55:41 +0900

    [DPMBE-108] 지역 키워드 검색시 검색 제한 해제한다
    * feat : 검색제한을 10개로 변경
    
    * refactor : test parameter 변경
    query, display -> display, query

--- Whatnow-Common/src/main/kotlin/com/depromeet/whatnow/consts/WhatNowStatic.kt
+++ Whatnow-Common/src/main/kotlin/com/depromeet/whatnow/consts/WhatNowStatic.kt
@@ -28,6 +28,7 @@ const val WITHDRAW_PREFIX = "withdraw"
 const val RADIUS_WAIT_CONFIRM = 200
 const val SLACK_MAX_LENGTH = 1000
 
+const val NCP_LOCAL_SEARCH_DISPLAY_COUNT = 10
 const val IMAGE_DOMAIN = "https://image.whatnow.kr"
 const val ASSERT_IMAGE_DOMAIN = "https://image.whatnow.kr/assert"
 const val USER_DEFAULT_PROFILE_IMAGE = "https://image.whatnow.kr/assert/users/default.svg"
