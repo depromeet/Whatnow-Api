3e84879ffcc202b31e8c02b446fced8eda6a6842
이서현 <ymecca12@gmail.com>
2023-07-13 18:52:32 +0900

    feat : GeoMeter 기반 거리 측정으로 변경

--- Whatnow-Domain/build.gradle.kts
+++ Whatnow-Domain/build.gradle.kts
@@ -5,6 +5,8 @@ dependencies {
     api("org.springframework.boot:spring-boot-starter-data-redis")
     api("com.mysql:mysql-connector-j")
     implementation("com.google.geometry:s2-geometry:2.0.0")
+    //geohash
+    implementation("ch.hsr:geohash:1.4.0")
     implementation(project(":Whatnow-Infrastructure"))
     implementation(project(":Whatnow-Common"))
     api("com.h2database:h2")
--- Whatnow-Domain/src/main/kotlin/com/depromeet/whatnow/events/handler/PromiseUserUpdateLocationEventHandler.kt
+++ Whatnow-Domain/src/main/kotlin/com/depromeet/whatnow/events/handler/PromiseUserUpdateLocationEventHandler.kt
@@ -1,5 +1,8 @@
 package com.depromeet.whatnow.events.handler
 
+import ch.hsr.geohash.GeoHash
+import com.depromeet.whatnow.common.vo.CoordinateVo
+import com.depromeet.whatnow.consts.RADIUS_CONVERT_METER
 import com.depromeet.whatnow.domains.promise.adaptor.PromiseAdaptor
 import com.depromeet.whatnow.domains.promiseuser.adaptor.PromiseUserAdaptor
 import com.depromeet.whatnow.domains.promiseuser.service.PromiseUserDomainService
@@ -10,13 +13,14 @@ import org.springframework.transaction.annotation.Propagation
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
-
 @Component
 class PromiseUserUpdateLocationEventHandler(
     val promiseAdaptor: PromiseAdaptor,
     val promiseUserAdaptor: PromiseUserAdaptor,
     val promiseUserDomainService: PromiseUserDomainService,
 ) {
+    // logger
+
     @Async
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     @TransactionalEventListener(classes = [PromiseUserUpdateLocationEvent::class], phase = TransactionPhase.AFTER_COMMIT)
@@ -24,7 +28,8 @@ class PromiseUserUpdateLocationEventHandler(
         val promiseId = promiseUserUpdateLocationEvent.promiseId
         val promise = promiseAdaptor.queryPromise(promiseId)
         val promiseUser = promiseUserAdaptor.findByPromiseIdAndUserId(promiseUserUpdateLocationEvent.promiseId, promiseUserUpdateLocationEvent.userId)
-        if (promiseUserDomainService.isArrived(promiseUser, promise.meetPlace!!.coordinate)) {
+//        if (promiseUserDomainService.isArrived(promiseUser, promise.meetPlace!!.coordinate)) {
+        if (isArrived(promiseUser.userLocation, promise.meetPlace!!.coordinate)) {
             // 활성화된 약속이 종료되기 전일 때
             if (promise.isBeforePromiseEndTime() && promise.isActive()) {
                 // 약속유저 상태 도착 상태(WAIT)로 변경
@@ -32,5 +37,33 @@ class PromiseUserUpdateLocationEventHandler(
             }
             // 활성화된 약속이 종료되고 나서 도착 시 사전에 LATE처리를 해서 별도의 처리가 필요 없음
         }
+    }
+
+    fun isArrived(promiseUser: CoordinateVo, coordinate: CoordinateVo): Boolean {
+        val start = GeoHash.withCharacterPrecision(promiseUser.latitude, promiseUser.longitude, 10).toBase32()
+        val end = GeoHash.withCharacterPrecision(coordinate.latitude, coordinate.longitude, 10).toBase32()
+        val distance = calculateDistanceUsingGeoHash(start, end)
+        return distance <= 20
+    }
+
+    fun calculateDistanceUsingGeoHash(geoHash1: String, geoHash2: String): Double {
+        val commonPrefixLength = getCommonPrefixLength(geoHash1, geoHash2)
+        val cellSize = getCellSize(commonPrefixLength)
+        return cellSize * RADIUS_CONVERT_METER // Convert to meters
+    }
+
+    fun getCommonPrefixLength(geoHash1: String, geoHash2: String): Int {
+        var commonPrefixLength = 0
+        val length = minOf(geoHash1.length, geoHash2.length)
+
+        while (commonPrefixLength < length && geoHash1[commonPrefixLength] == geoHash2[commonPrefixLength]) {
+            commonPrefixLength++
+        }
+
+        return commonPrefixLength
+    }
+
+    fun getCellSize(commonPrefixLength: Int): Double {
+        return 180.0 / (1 shl (commonPrefixLength * 2))
     }
 }
